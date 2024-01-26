d105eba04979b943c284ab1b0c682fc1665a2ccd
cru23110 <cru23110@uvg.edu.gt>
2024-01-26 08:39:44 -0600

    The logic of the custom stack was created

---
+++ VlastnýZásobník.java
@@ -0,0 +1,35 @@
+public class VlastnýZásobník<T> {
+    private Uzol<T> vrch;
+
+    public VlastnýZásobník() {
+        this.vrch = null;
+    }
+
+    public boolean jePrazdny() {
+        return vrch == null;
+    }
+
+    public void vlož(T data) {
+        Uzol<T> novýUzol = new Uzol<>(data);
+        novýUzol.setNext(vrch);
+        vrch = novýUzol;
+    }
+
+    public T vyber() {
+        if (jePrazdny()) {
+            throw new IllegalStateException("Zásobník je prázdny");
+        }
+        T data = vrch.getData();
+        vrch = vrch.getNext();
+        return data;
+    }
+
+    public T vrchol() {
+        if (jePrazdny()) {
+            throw new IllegalStateException("Zásobník je prázdny");
+        }
+        return vrch.getData();
+    }
+}
+
+
