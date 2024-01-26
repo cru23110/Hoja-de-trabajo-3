068573f2159efaefba9e4dff3ae9eb0eef8f9555
cru23110 <cru23110@uvg.edu.gt>
2024-01-26 09:37:41 -0600

    The logic was added to generate the csv with the data and generate the graph in Excel

--- GrafickyPloter.java
+++ GrafickyPloter.java
@@ -1,3 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class GrafickyPloter {
 
+    private static final String NOMBRE_ARCHIVO = "resultados.csv";
+
+    public static void generarCSV(String algoritmo, int cantidadNumeros, long tiempo) {
+        try {
+            boolean existeArchivo = Files.exists(Paths.get(NOMBRE_ARCHIVO));
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(NOMBRE_ARCHIVO, true))) {
+                // Si el archivo no existe, agregar encabezado
+                if (!existeArchivo) {
+                    writer.write("Algoritmo,Cantidad de Números,Tiempo\n");
+                }
+
+                // Escribir datos en el archivo
+                writer.write(algoritmo + "," + cantidadNumeros + "," + tiempo + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
