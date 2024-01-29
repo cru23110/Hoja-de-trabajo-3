9944094dfea1014636998159cd5a8d8a712ed8d6
bar23354 <bar23354@uvg.edu.gt>
2024-01-29 10:08:32 -0600

    Num generator and Main

--- GeneratorNahodnychCisel.java
+++ GeneratorNahodnychCisel.java
@@ -1,3 +1,12 @@
+import java.util.Random;
+
 public class GeneratorNahodnychCisel {
-    
+    public int[] generateNumbers(int amount) {
+        Random random = new Random();
+        int[] numbers = new int[amount];
+        for (int i = 0; i < amount; i++) {
+            numbers[i] = 1 + random.nextInt(3000);
+        }
+        return numbers;
+    }
 }
--- Hlavna.java
+++ Hlavna.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Hlavna {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the amount of numbers to generate:");
+        int amount = scanner.nextInt();
+
+        GeneratorNahodnychCisel generator = new GeneratorNahodnychCisel();
+        int[] numbers = generator.generateNumbers(amount);
+
+        try (PrintWriter writer = new PrintWriter(new File("GoldenTicket.txt"))) {
+            for (int number : numbers) {
+                writer.println(number);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred while trying to write to the file.");
+            e.printStackTrace();
+        }
     }
 }
